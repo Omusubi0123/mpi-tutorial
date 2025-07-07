19eaf9851ec43ad401c4c37f65624f34361cf5d0
Omusubi0123 <Omusubi0123@github.com>
2025-07-07 21:19:22 +0900

    feat: ノードグラフ可視化を見やすく

--- tutorial/pagerank/preprocess/visualize.py
+++ tutorial/pagerank/preprocess/visualize.py
@@ -19,10 +19,10 @@ def visualize_graph(load_file: str, save_file: str, sample_nodes: int = -1):
     H = G.subgraph(sampled_nodes)
 
     plt.figure(figsize=(10, 10))
-    nx.draw_networkx(H, node_size=30, arrowsize=10, with_labels=True)
+    nx.draw_networkx(H, node_size=400, arrowsize=30, with_labels=True)
     # pos = nx.spring_layout(H, k=0.5, seed=42)  # レイアウトを調整してばらけさせる
     # nx.draw_networkx(H, pos=pos, node_size=30, arrowsize=10, with_labels=True)
-    plt.title("Sampled Graph View")
+    plt.title(f"Sampled Graph View ({len(sampled_nodes)} nodes)")
     plt.savefig(save_file, dpi=300)
 
 

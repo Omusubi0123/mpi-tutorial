119d91ced4ba2b89da613e0d984256d3406d3ada
Omusubi0123 <Omusubi0123@github.com>
2025-07-07 16:52:22 +0900

    feat: PageRankをnaiveな行列で実装

--- tutorial/pagerank/preprocess/generate_graph.py
+++ tutorial/pagerank/preprocess/generate_graph.py
@@ -1,16 +1,19 @@
 # tutorial/pagerank/で実行すること
-
+import os
 import random
 
 def generate_large_graph(n_nodes: int, avg_degree: int, filename: str):
+    if not os.path.exists(os.path.dirname(filename)):
+        os.mkdir(os.path.dirname(filename))
     with open(filename, "w") as f:
+        f.write(f"{n_nodes}\n")
         for src in range(n_nodes):
             targets = random.sample(range(n_nodes), avg_degree + random.randint(-avg_degree, avg_degree))
             for tgt in targets:
                 f.write(f"{src} {tgt}\n")
 
 if __name__ == "__main__":
-    n_nodes = 100
-    avg_degree = 5
-    filename = "data/graph.txt"
+    n_nodes = 10
+    avg_degree = 4
+    filename = f"data/{n_nodes}/graph.txt"
     generate_large_graph(n_nodes=n_nodes, avg_degree=avg_degree, filename=filename)
--- tutorial/pagerank/preprocess/visualize.py
+++ tutorial/pagerank/preprocess/visualize.py
@@ -3,24 +3,30 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-def visualize_graph(load_file: str, save_file: str, sample_nodes: int):
+def visualize_graph(load_file: str, save_file: str, sample_nodes: int = -1):
     G = nx.DiGraph()
     with open(load_file, "r") as f:
+        n = int(f.readline().strip())
         for line in f:
             u, v = map(int, line.strip().split())
             G.add_edge(u, v)
 
-    sampled_nodes = random.sample(list(G.nodes), sample_nodes)
+    if sample_nodes != -1:
+        sampled_nodes = random.sample(list(G.nodes), sample_nodes)
+    else:
+        sampled_nodes = list(G.nodes)
     H = G.subgraph(sampled_nodes)
 
     plt.figure(figsize=(10, 10))
-    pos = nx.spring_layout(H, k=0.5, seed=42)  # レイアウトを調整してばらけさせる
-    nx.draw_networkx(H, pos=pos, node_size=30, arrowsize=10, with_labels=True)
+    nx.draw_networkx(H, node_size=30, arrowsize=10, with_labels=True)
+    # pos = nx.spring_layout(H, k=0.5, seed=42)  # レイアウトを調整してばらけさせる
+    # nx.draw_networkx(H, pos=pos, node_size=30, arrowsize=10, with_labels=True)
     plt.title("Sampled Graph View")
     plt.savefig(save_file, dpi=300)
 
 
 if __name__ == "__main__":
-    load_file = "data/graph.txt"
-    save_file = "data/graph.png"
-    visualize_graph(load_file=load_file, save_file=save_file, sample_nodes=100)
+    n_node = 10
+    load_file = f"data/{n_node}/graph.txt"
+    save_file = f"data/{n_node}/graph.png"
+    visualize_graph(load_file=load_file, save_file=save_file)
